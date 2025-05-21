6d7a9cec52b8863282fde9399f5ff80f53e34ac4
marguberk <your.email@example.com>
2025-05-21 21:34:55 +0500

    fix: корректный reverse для tasks, исправлено создание задач и уведомлений

--- core/models.py
+++ core/models.py
@@ -62,7 +62,7 @@ class Notification(models.Model):
             type=cls.Type.TASK,
             title=title,
             message=message,
-            link=reverse('tasks')
+            link=reverse('core:tasks')
         )
 
     @classmethod
